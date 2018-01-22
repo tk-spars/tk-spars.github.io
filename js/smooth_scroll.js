2ba85137eb90bfb5ab166ffb3755d0b0ae20c7d0
nilsmorozs <nilsmorozs@gmail.com>
2018-01-22 00:39:28 +0000

    Updated design with a logo-only home screen

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#homepage']").on('click', function(event) {
+  $("a, .navbar a, footer a[href='#homepage']").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
